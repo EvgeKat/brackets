d2906a2b5274080756468eef1c760e2b319407de
EvgeKat <katargina_evg@mail.ru>
2022-02-06 20:44:51 +0300

    Signed-off-by: EvgeKat <katargina_evg@mail.ru>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   
+  let stack = [];
+  let brackets = new Map(bracketsConfig);
+
+  for (let i = 0; i < str.length; i++) {
+     
+      if (str[i] == brackets.get(stack[stack.length - 1])) {
+        stack.pop();
+      } else {
+          stack.push(str[i]);
+        }
+        
+    }
+    return stack.length !== 0 ? false : true;
 }
+
+
+ 
+
+    
